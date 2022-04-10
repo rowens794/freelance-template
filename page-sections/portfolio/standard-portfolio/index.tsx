6d29db55da1e6e70129dbf88f0d7d89f56098afa
rowens794 <rowens794@gmail.com>
2022-04-10 19:05:46 -0400

    added blur + priority to portfolio images

--- page-sections/portfolio/standard-portfolio/index.tsx
+++ page-sections/portfolio/standard-portfolio/index.tsx
@@ -6,7 +6,9 @@ const posts = [
     category: 'Marketing Site',
     description:
       'Wellington is a brochure site for an Investment Management company that helps individuals, companies, and nonprofit organizations manage & invest financial assets so that they can meet their long-term goals.',
-    pageImg: '/images/wellington.png',
+    pageImg: '/images/wellington.jpg',
+    blurImg: '/images/wellington-blur.jpg',
+
     imageUrl: '/images/Wellington.jpg',
     buttonText: 'Visit Wellington',
     siteLink: 'https://demo-ria-site.netlify.app/',
@@ -19,7 +21,8 @@ const posts = [
     category: 'Marketing Site',
     description:
       "Allison's is a Public Accounting office that focuses primarily on tax work.  As customers have become more and more tech-savvy, this business needed a web presence that showed up on Google alongside some of the major national players. ",
-    pageImg: '/images/logoipsum.png',
+    pageImg: '/images/logoipsum.jpg',
+    blurImg: '/images/logoipsum-blur.jpg',
     imageUrl: '/images/wellingtonScreen.jpg',
     buttonText: "Visit Allison's",
     siteLink: 'https://tax-site-staging.netlify.app/',
@@ -32,7 +35,9 @@ const posts = [
     category: 'Web Application',
     description:
       "Intelispect is a Software as a Service business that needed a front and backend that would help them display information on more than 1.5 million nonprofit organizations in the US. They're site built using some of the latest web technologies to ensure that it's fast, stable, and easy to use.",
-    pageImg: '/images/intellispect.png',
+    pageImg: '/images/intellispect.jpg',
+    blurImg: '/images/intellispect-blur.jpg',
+
     imageUrl: '/images/wellingtonScreen.jpg',
     buttonText: 'Visit Intellispect',
     siteLink: 'https://www.intellispect.co',
@@ -127,15 +132,18 @@ const NewPost = ({ post }: { post: any }) => {
             width={post.width / 4}
             height={post.height / 4}
             className="w-full"
+            blurDataURL={post.blurUrl}
+            priority={true}
+            loading="eager"
           />
         </div>
       </div>
 
-      <div className="absolute z-30 w-full mx-auto bottom-6 ">
-        <p className="relative z-40 mx-auto w-full max-w-[495px] bg-primary-800 px-2 pt-4 text-center text-xl font-light text-white opacity-90">
+      <div className="absolute z-30 w-full mx-auto bottom-6 md:bottom-14 ">
+        <p className="relative z-40 mx-auto w-full max-w-[495px] bg-primary-800 px-2 pt-2 text-center text-lg font-light text-white opacity-90">
           {post.title}
         </p>
-        <p className="relative z-40 mx-auto w-full max-w-[495px] bg-primary-800 px-2 pb-4 text-center text-sm font-light text-white opacity-90">
+        <p className="relative z-40 mx-auto w-full max-w-[495px] bg-primary-800 px-2 pb-2 text-center text-sm font-light text-white opacity-90">
           {post.category}
         </p>
       </div>
